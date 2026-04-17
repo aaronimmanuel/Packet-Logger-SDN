9fa43d2be4685cd2b3cbff852b68875f76089ea2
Aaron Immanuel <aaronimmanuel707@gmail.com>
2026-04-17 10:51:18 +0530

    Refactor packet logger for SDN with POX

--- packet_logger.py
+++ packet_logger.py
@@ -1,55 +1,93 @@
-from mininet.net import Mininet
-from mininet.node import RemoteController, OVSSwitch
-from mininet.cli import CLI
-from mininet.log import setLogLevel
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from datetime import datetime
 
+log = core.getLogger()
 
-def run_topology():
-    """
-    Simple SDN topology:
-    3 hosts + 1 switch + POX controller
-    """
+class PacketLogger(object):
 
-    net = Mininet(controller=RemoteController, switch=OVSSwitch)
+    def __init__(self, connection):
+        self.connection = connection
+        self.mac_to_port = {}
+        self.packet_count = 0
 
-    print("*** Adding Controller")
-    c0 = net.addController(
-        'c0',
-        controller=RemoteController,
-        ip='127.0.0.1',
-        port=6633
-    )
+        self.log_file = "/home/aaron/Desktop/packet_logger_sdn/packet_logs.txt"
 
-    print("*** Adding Hosts")
-    h1 = net.addHost('h1', ip='10.0.0.1/24')
-    h2 = net.addHost('h2', ip='10.0.0.2/24')
-    h3 = net.addHost('h3', ip='10.0.0.3/24')
+        with open(self.log_file, "w") as f:
+            f.write("===== SDN PACKET LOGGER =====\n\n")
 
-    print("*** Adding Switch")
-    s1 = net.addSwitch('s1', protocols='OpenFlow10')
+        connection.addListeners(self)
 
-    print("*** Creating Links")
-    net.addLink(h1, s1)
-    net.addLink(h2, s1)
-    net.addLink(h3, s1)
+    def write_log(self, text):
+        with open(self.log_file, "a") as f:
+            f.write(text + "\n")
 
-    print("*** Starting Network")
-    net.start()
+    def _handle_PacketIn(self, event):
 
-    print("\n*** Network Ready ***")
-    print("Hosts available: h1, h2, h3")
-    print("Try commands like:")
-    print("  h1 ping h2")
-    print("  h1 python3 -m http.server 80")
-    print("  h2 curl 10.0.0.1")
-    print("  exit\n")
+        packet = event.parsed
+        if not packet.parsed:
+            return
 
-    CLI(net)
+        self.packet_count += 1
 
-    print("*** Stopping Network")
-    net.stop()
+        src_mac = str(packet.src)
+        dst_mac = str(packet.dst)
+        in_port = event.port
 
+        protocol = "OTHER"
 
-if __name__ == '__main__':
-    setLogLevel('info')
-    run_topology()
+        if packet.find('arp'):
+            protocol = "ARP"
+        elif packet.find('icmp'):
+            protocol = "ICMP"
+        elif packet.find('tcp'):
+            protocol = "TCP"
+        elif packet.find('udp'):
+            protocol = "UDP"
+
+        ip_pkt = packet.find('ipv4')
+
+        if ip_pkt:
+            src_ip = str(ip_pkt.srcip)
+            dst_ip = str(ip_pkt.dstip)
+
+            if src_ip == "10.0.0.3" and dst_ip == "10.0.0.1":
+                log.info("BLOCKED: h3 → h1")
+
+                drop_msg = of.ofp_flow_mod()
+                drop_msg.priority = 100
+                drop_msg.match.dl_type = 0x0800
+                drop_msg.match.nw_src = ip_pkt.srcip
+                drop_msg.match.nw_dst = ip_pkt.dstip
+                event.connection.send(drop_msg)
+
+                return
+
+        self.mac_to_port[src_mac] = in_port
+
+        if dst_mac in self.mac_to_port:
+            out_port = self.mac_to_port[dst_mac]
+        else:
+            out_port = of.OFPP_FLOOD
+
+        log_text = f"""
+Packet #{self.packet_count}
+Time: {datetime.now()}
+Protocol: {protocol}
+Source MAC: {src_mac}
+Destination MAC: {dst_mac}
+"""
+
+        print(log_text)
+        self.write_log(log_text)
+
+        msg = of.ofp_packet_out()
+        msg.data = event.ofp
+        msg.actions.append(of.ofp_action_output(port=out_port))
+        event.connection.send(msg)
+
+def launch():
+    def start_switch(event):
+        PacketLogger(event.connection)
+
+    core.openflow.addListenerByName("ConnectionUp", start_switch)
